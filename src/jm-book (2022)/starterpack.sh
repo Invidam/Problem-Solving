6c6ff15cbca800261f24f0f107c2fe8df39b62fe
Invidam <hansupark0208@gmail.com>
2022-02-16 11:16:13 +0900

    feat: Change shell files name.
    2022.2.16.
    {Problem site}.sh -> starterpack.sh
    This file is in directory divided problem sites. so problem site's name is not needed.

--- src/jm-book (2022)/starterpack.sh
+++ src/jm-book (2022)/starterpack.sh
@@ -3,11 +3,10 @@ mkdir $1
 echo "#include <iostream>
 using namespace std;
 
+//Problem site: https://algospot.com/judge/problem/read/$1
+
 int main() {
-    freopen(\"input.txt\", \"r\", stdin);
 
     return 0;
 }
 " > $1/$1.cc
-
-echo "" > $1/input.txt
--- src/letcode/starterpack.sh
+++ src/letcode/starterpack.sh
@@ -3,12 +3,10 @@ mkdir $1
 echo "#include <iostream>
 using namespace std;
 
+// Problem site: https://leetcode.com/problems/$1
 
 int main() {
-    freopen(\"input.txt\", \"r\", stdin);
 
     return 0;
 }
 " > $1/$1.cc
-
-echo "" > $1/input.txt
