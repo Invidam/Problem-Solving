ca941aacad5c81154b5c0e1551070cbc83aea276
Invidam <hansupark0208@gmail.com>
2022-03-13 20:07:17 +0900

    feat: update leetcode's add-two-number-2

--- src/leetcode/add-two-numbers/add-two-numbers-2.cc
+++ src/leetcode/add-two-numbers/add-two-numbers-2.cc
@@ -1,82 +1,19 @@
 #include <iostream>
+#include <limits>
+
 using namespace std;
 
-// Problem site: https://leetcode.com/problems/add-two-numbers
-
-//  Definition for singly-linked list.
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-int valueOf(ListNode *node)
-{
-    if (node == nullptr)
-        return 0;
-    else
-        return node->val;
-}
-
-void print_node(ListNode *node)
-{
-    ListNode *itr = node;
-    while (itr != nullptr)
-    {
-        cout << itr->val << " ";
-        itr = itr->next;
-    }
-    cout << "\n";
-    return;
-}
-class Solution
-{
-public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
-    {
-        ListNode *answerPrev = new ListNode();
-        ListNode *itr = answerPrev;
-        int up = 0;
-        while (l1 || l2)
-        {
-            int sum = up;
-            if (l1)
-            {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if (l2)
-            {
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            up = sum / 10;
-            itr->next = new ListNode(sum % 10);
-            itr = itr->next;
-        }
-        if (up)
-            itr->next = new ListNode(up);
-        return answerPrev->next;
-    }
-};
 int main()
 {
+    double a{std::numeric_limits<double>::min()};
 
-    // 3947
-    // 0666
-    // 4613
-    ListNode n4 = ListNode(3, nullptr);
-    ListNode n3 = ListNode(9, &n4);
-    ListNode n2 = ListNode(4, &n3);
-    ListNode n1 = ListNode(7, &n2);
-
-    ListNode _n3 = ListNode(6, nullptr);
-    ListNode _n2 = ListNode(6, &_n3);
-    ListNode _n1 = ListNode(6, &_n2);
-    ListNode *ans = Solution().addTwoNumbers(&n1, &_n1);
-    print_node(ans);
-    return 0;
+    cout << a << "\n";
+    a = a / 100000000000000;
+    cout << a << " " << std::numeric_limits<double>::infinity() << "\n";
 }
+
+// q.1 2의보수 max에서 더했을떄 몇이되는지
+// 2. double min /10의 결과
+// 3.  변수 선언을 변수 이용하여 하기 (decltype)
+//  4. 배열변수의 참조선언
+//  5. 강제 형변환 축소변환 이해
