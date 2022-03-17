80cd9315a909ff7ddce5303c3a5a998964c36595
Invidam <hansupark0208@gmail.com>
2022-03-17 23:55:57 +0900

    feat: update leetcode's score-of-parentheses

---
+++ src/leetcode/score-of-parentheses/score-of-parentheses.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// Problem site: https://leetcode.com/problems/score-of-parentheses
+class Solution
+{
+public:
+    int scoreOfParentheses(string s)
+    {
+        const int OPEN = -1;
+        const int CLOSE = -2;
+        stack<int> st;
+        for (auto &elem : s)
+        {
+            if (elem != '(' && elem != ')')
+                st.push(elem - '0');
+            else if (elem == '(')
+                st.push(OPEN);
+            else
+            {
+                int num = 1;
+                int sum = 0;
+                while (st.top() != OPEN)
+                {
+                    sum += 2 * st.top();
+                    st.pop();
+                }
+                st.pop();
+                st.push(sum == 0 ? num : sum);
+            }
+        }
+
+        int ret = 0;
+        while (!st.empty())
+        {
+            ret += st.top();
+            st.pop();
+        }
+        return ret;
+    }
+};
+int main()
+{
+    //"(((8()())))"
+    cout << Solution().scoreOfParentheses("(((811)))") << '\n';
+    return 0;
+}
